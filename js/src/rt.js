57744ad4f520fc35d17dde162c5c6f76689bdcfc
Yu Changyuan <reivzy@gmail.com>
2020-02-12 17:40:46 +0800

    js/rt: export is_nodejs.

--- js/src/rt.js
+++ js/src/rt.js
@@ -1,5 +1,11 @@
 // update when update seia-VER.js
 module.exports.VERSION = 1;
 
+function is_nodejs() {
+    return (typeof(window._rt.cwd) == 'string');
+}
+
+
+module.exports.is_nodejs = is_nodejs;
 module.exports.mqtt  = require('mqtt');
 module.exports.store = require('./store');
