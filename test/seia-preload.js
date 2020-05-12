2327c428563c614304a139fabdbef015b7b957e4
Yu Changyuan <reivzy@gmail.com>
2020-05-12 10:25:15 +0800

    up seq to 8, use longer timeout.

--- test/seia-preload.js
+++ test/seia-preload.js
@@ -164,7 +164,7 @@ module.exports.load = load;
 
 },{"./store":6}],3:[function(require,module,exports){
 // update when update seia-SEQ.js
-SEQ = 7;
+SEQ = 8;
 
 // min seq we can accept
 SEQ_MIN = 7;
