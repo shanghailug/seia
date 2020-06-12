5f7623e47826a9cb1baf9005643bd80040f4c38f
Yu Changyuan <reivzy@gmail.com>
2020-06-13 00:28:04 +0800

    up seq to 10, try to avoid resource leak

--- test/seia-preload.js
+++ test/seia-preload.js
@@ -164,7 +164,7 @@ module.exports.load = load;
 
 },{"./store":6}],3:[function(require,module,exports){
 // update when update seia-SEQ.js
-SEQ = 9;
+SEQ = 10;
 
 // min seq we can accept
 SEQ_MIN = 7;
